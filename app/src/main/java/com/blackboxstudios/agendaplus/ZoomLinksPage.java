48b7ff2812af99b85d238ea2d0c546a2bbe55d93
Saurav <38475004+SauravC99@users.noreply.github.com>
2020-10-09 18:45:10 -0700

    Implemented persistent layout for Buttons

--- app/src/main/java/com/blackboxstudios/agendaplus/ZoomLinksPage.java
+++ app/src/main/java/com/blackboxstudios/agendaplus/ZoomLinksPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +13,18 @@ import android.widget.Toast;
 
 public class ZoomLinksPage extends AppCompatActivity {
     // Created an int array of Button ids
+    //buttonNames contains the IDs of the buttons
     final int[] buttonNames =
             {R.id.button, R.id.button4, R.id.button5, R.id.button6,
                     R.id.button7, R.id.button8, R.id.button9, R.id.button10};
+
+    //buttonList contains the Button objects themselves
     Button[] buttonList = new Button[buttonNames.length - 1];
-    int count = 0;
+    int count = 0; //Count variable to keep track of visible buttons
+    String COUNT = "COUNT";
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +34,41 @@ public class ZoomLinksPage extends AppCompatActivity {
         // Created a list of Buttons and added the buttons on screen
         // We are doing i + 1 on line 26 cause buttonNames has 7, but buttonList has 6
         for (int i = 0; i < buttonList.length; i++) {
-            buttonList[i] = findViewById(buttonNames[i+1]);
+            buttonList[i] = findViewById(buttonNames[i + 1]);
+        }
+        pref = getPreferences(MODE_PRIVATE);
+        //Check if the button has been made visible by the user and set layout
+        for (Button b : buttonList) {
+            if (pref.contains(Integer.toString(b.getId()))) {
+                int vis = pref.getInt(Integer.toString(b.getId()), View.GONE);
+                b.setVisibility(vis);
+            }
         }
     }
-    
+
     /** Called when the user taps the Add Class Button */
     public void addClass(View view) {
+        pref = getPreferences(MODE_PRIVATE);
+        editor = pref.edit();
+        //editor.remove("COUNT");
+        //Get the num the counter was at last time user used this screen
+        if (pref.contains(COUNT)) {
+            count = pref.getInt(COUNT, 0);
+        }
         // Add 1 to count bc we want to know another button is shown unless its at max
-        if (count < buttonList.length)
+        if (count < buttonList.length) {
             count++;
+            editor.putInt(COUNT, count);
+            editor.apply();
+            //editor.clear();
+        }
+        //Iterate up to count and make those buttons visible if they are not
         for (int i = 0; i < count; i++) {
             if (buttonList[i].getVisibility() == View.GONE) {
                 buttonList[i].setVisibility(View.VISIBLE);
+                editor.putInt(getString(buttonNames[i+1]), buttonList[i].getVisibility());
+                editor.apply();
+                //editor.clear();
             }
         }
     }
@@ -75,9 +106,16 @@ public class ZoomLinksPage extends AppCompatActivity {
     private void saveClassDetails(String data, View view) {
         Button a = findViewById(view.getId());
         a.setText(data);
-
         //Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
-
     }
 
+    //For testing - use this to reset saved values
+    public void clear(View view) {
+        pref = getPreferences(MODE_PRIVATE);
+        editor = pref.edit();
+        editor.clear();
+        count = 0;
+        editor.remove("COUNT");
+        editor.apply();
+    }
 }
