90e70e0dd7fc0bf60035c58cefd24f02746d555d
Edmund Zhou <edmundzhou97@gmail.com>
2020-11-01 23:39:51 -0800

    Update Calculator.java

--- app/src/main/java/com/blackboxstudios/agendaplus/Calculator.java
+++ app/src/main/java/com/blackboxstudios/agendaplus/Calculator.java
@@ -7,90 +7,129 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Calculator extends AppCompatActivity {
+public class Calculator extends AppCompatActivity
+{
+    //private declaration
     private TextView Screen;
-    private String input="",Answer;
-    private boolean clearResult;
+    private String input="";
+    private String Answer;
+    private boolean clearEmpty;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
         Screen=findViewById(R.id.screen);
     }
-    public void ButtonClick(View view){
+    public void ButtonClick(View view)
+    {
         Button button= (Button) view;
         String data=button.getText().toString();
-        switch (data){
+        switch (data)
+        {
             case "AC":
+                //clear
                 input="";
                 break;
             case "Ans":
-                clearResult=false;
+                //answer
+                clearEmpty=false;
+                //input = input + answer b4
                 input+=Answer;
                 break;
             case "x":
-                clearResult=false;
+                //multiply
+                clearEmpty=false;
                 Solve();
+                //solving function
                 input+="*";
+                //* is multiple
                 break;
             case "^":
-                clearResult=false;
+                clearEmpty=false;
+                //exponential
                 Solve();
+                //solve function
                 input+="^";
+                //^ symbol for exponential
                 break;
             case "=":
-                clearResult=true;
+                //equal
+                clearEmpty=true;
                 Solve();
+                //solve function
                 Answer=input;
+                //answer = input and input
                 break;
             case "⬅":
-                if(input.length()>0){
-                    clearResult=false;
+                //go back
+                if(input.length()>0)
+                {
+                    clearEmpty=false;
                     String newText=input.substring(0,input.length()-1);
+                    //go back a spot command
                     input=newText;
+                    //so new input
                 }
                 break;
+                //end
             default:
-                if(input==null){
+                if(input==null)
+                {
                     input="";
+                    //empty
                 }
-                if(data.equals("+") || data.equals("-") || data.equals("/")){
-                    clearResult=false;
+                if(data.equals("+") || data.equals("-") || data.equals("/"))
+                {
+                    clearEmpty=false;
                     Solve();
+                //solve function
                 }
-                else if(clearResult==true){
+                else if(clearEmpty==true)
+                {
                     input="";
-                    clearResult=false;
+                    clearEmpty=false;
                 }
                 input+=data;
         }
         Screen.setText(input);
     }
-    public void Solve(){
-        if(input.split("\\*").length==2){
+    public void Solve()
+            //solve function
+    {
+        if(input.split("\\*").length==2)
+        {
             String numbers[]=input.split("\\*");
-            try{
+            //try catch tester
+            try
+            {
                 double mul=Double.parseDouble(numbers[0])*Double.parseDouble(numbers[1]);
                 input=mul+"";
             }
-            catch (Exception e){
+            catch (Exception e)
+            {
                 //Display error
             }
         }
-        else if(input.split("/").length==2){
+        else if(input.split("/").length==2)
+        {
             String numbers[]=input.split("/");
-            try{
+            try
+            {
                 double div=Double.parseDouble(numbers[0])/Double.parseDouble(numbers[1]);
                 input=div+"";
             }
-            catch (Exception e){
+            catch (Exception e)
+            {
                 //Display error
             }
         }
-        else if(input.split("\\^").length==2){
+        else if(input.split("\\^").length==2)
+        {
             String numbers[]=input.split("\\^");
-            try{
+            try
+            {
                 double pow=Math.pow(Double.parseDouble(numbers[0]),Double.parseDouble(numbers[1]));
                 input=pow+"";
             }
@@ -98,38 +137,49 @@ public class Calculator extends AppCompatActivity {
                 //Display error
             }
         }
-        else if(input.split("\\+").length==2){
+        else if(input.split("\\+").length==2)
+        {
             String numbers[]=input.split("\\+");
-            try{
+            try
+            {
                 double sum=Double.parseDouble(numbers[0])+Double.parseDouble(numbers[1]);
                 input=sum+"";
             }
-            catch (Exception e){
-                //Display error
+            catch (Exception e)
+            {
+                //Show the error
             }
         }
-        else if(input.split("\\-").length>1){
+        else if(input.split("\\-").length>1)
+        {
             String numbers[]=input.split("\\-");
-            if(numbers[0]=="" && numbers.length==2){
+            if(numbers[0]=="" && numbers.length==2)
+            {
                 numbers[0]=0+"";
             }
-            try{
+            try
+            {
                 double sub=0;
-                if(numbers.length==2) {
+                if(numbers.length==2)
+                {
                     sub = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
                 }
-                else if(numbers.length==3){
+                else if(numbers.length==3)
+                {
                     sub = -Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[2]);
                 }
                 input=sub+"";
             }
-            catch (Exception e){
-                //Display error
+            catch (Exception e)
+            {
+                //Show the error
             }
         }
         String numbers[]=input.split("\\.");
-        if(numbers.length>1){
-            if(numbers[1].equals("0")){
+        if(numbers.length>1)
+        {
+            if(numbers[1].equals("0"))
+            {
                 input=numbers[0];
             }
         }
