e92220d80f2e7fc7eceddaf5a651251eed2a48a2
Saurav <38475004+SauravC99@users.noreply.github.com>
2020-11-17 01:36:28 -0800

    Added methods in the database helper to add, delete, and get contents

--- app/src/main/java/com/blackboxstudios/agendaplus/ZoomLinksDatabaseHelper.java
+++ app/src/main/java/com/blackboxstudios/agendaplus/ZoomLinksDatabaseHelper.java
@@ -39,5 +39,86 @@ public class ZoomLinksDatabaseHelper extends SQLiteOpenHelper {
 
     }
 
-    
+    // Use getWritableDatabase when the db is changed
+    // Use getReadableDatabase when data is being read
+
+    public boolean addSingleClass(ZoomClassModel model) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(BUTTON_NUMBER, model.getButtonNum());
+        cv.put(COLUMN_CLASS_NAME, model.getClassName());
+        cv.put(COLUMN_ZOOM_LINK, model.getLink());
+
+        long insert = db.insert(ZOOMLINKSPAGE, null, cv);
+
+        db.close();
+        if(insert == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean deleteSingleClass(ZoomClassModel model) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + ZOOMLINKSPAGE + " WHERE " + BUTTON_NUMBER + " = " + model.getButtonNum();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            cursor.close();
+            db.close();
+            return true;
+        }
+        else {
+            cursor.close();
+            db.close();
+            return false;
+        }
+    }
+
+    public ZoomClassModel getSingleClass(int buttonNum) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + ZOOMLINKSPAGE + " WHERE " + BUTTON_NUMBER + " = " + buttonNum;
+
+        Cursor cursor = db.rawQuery(query, null);
+        ZoomClassModel model = new ZoomClassModel(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
+        return model;
+    }
+
+    public List<ZoomClassModel> getEverything() {
+
+        List<ZoomClassModel> ev = new ArrayList<>();
+
+        // Get everything from database
+        String query = "SELECT * FROM " + ZOOMLINKSPAGE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Gets the result set from the SQL statement
+        Cursor cursor = db.rawQuery(query, null);
+
+        // Go to the first line
+        if (cursor.moveToFirst()) {
+            // Go through results and make objects and add to list
+            do {
+                int buttonNumber = cursor.getInt(0);
+                String className = cursor.getString(1);
+                String link = cursor.getString(2);
+
+                ZoomClassModel newObj = new ZoomClassModel(buttonNumber, className, link);
+                ev.add(newObj);
+            } while (cursor.moveToNext()); // keep going until we reach the end of the DB
+        }
+        else {
+            // don't add anything to the list
+            //remove
+        }
+
+        // close things after we finish
+        cursor.close();
+        db.close();
+        return ev;
+    }
 }
