cc05c84a94f032eedd1063cc8428fc3a1c1c6558
Saurav <38475004+SauravC99@users.noreply.github.com>
2020-11-30 17:11:09 -0800

    Added testing home page

--- app/src/main/java/com/blackboxstudios/agendaplus/LogInPage.java
+++ app/src/main/java/com/blackboxstudios/agendaplus/LogInPage.java
@@ -22,14 +22,16 @@ public class LogInPage extends AppCompatActivity {
     /** Called when the user taps the Sign In button */
     public void signIn(View view) {
         //Intent intent = new Intent(this, StartUpPage.class);
-        Intent intent = new Intent(this, HomePage_Navigation.class);
+        //Intent intent = new Intent(this, HomePage_Navigation.class);
         //ed edited - change to work onto homepage 11/26
 
+        Intent intent = new Intent(this, zzTestHome.class);
+
         //Intent intent = new Intent(this, CalendarActivity.class);
-        EditText userName = (EditText) findViewById(R.id.editTextEmailAddress);
-        EditText password = (EditText) findViewById(R.id.editTextPassword);
-        String messageUser = userName.getText().toString();
-        String messagePass = password.getText().toString();
+        //EditText userName = (EditText) findViewById(R.id.editTextEmailAddress);
+        //EditText password = (EditText) findViewById(R.id.editTextPassword);
+        //String messageUser = userName.getText().toString();
+        //String messagePass = password.getText().toString();
         //intent.putExtra(USERNAME_MESSAGE, messageUser);
         //intent.putExtra(PASSWORD_MESSAGE, messagePass);
 
---
+++ app/src/main/java/com/blackboxstudios/agendaplus/zzTestHome.java
@@ -0,0 +1,41 @@
+package com.blackboxstudios.agendaplus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class zzTestHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zztesthome);
+    }
+
+    public void a(View view) {
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        startActivity(intent);
+    }
+    public void b(View view) {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+    public void c(View view) {
+        Intent intent = new Intent(this, ZoomLinksPage.class);
+        startActivity(intent);
+    }
+    public void d(View view) {
+        Intent intent = new Intent(this, LogInPage.class);
+        startActivity(intent);
+    }
+    public void e(View view) {
+        Intent intent = new Intent(this, GuestLogInPage.class);
+        startActivity(intent);
+    }
+    public void f(View view) {
+        Intent intent = new Intent(this, HomePage_Navigation.class);
+        startActivity(intent);
+    }
+}
