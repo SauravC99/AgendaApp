24b56b028e9fc12f53ef0a8f3f181d29e0f522c1
Tristen Seng <42958046+tristenseng@users.noreply.github.com>
2020-12-06 02:00:09 -0800

    AlarmPage

---
+++ app/src/main/java/com/blackboxstudios/agendaplus/AlarmPage.java
@@ -0,0 +1,54 @@
+package com.blackboxstudios.agendaplus;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.Calendar;
+
+
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmPage extends AppCompatActivity {
+    TimePicker timePicker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_page);
+        timePicker = findViewById(R.id.timePicker);
+        findViewById(R.id.buttonSetAlarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal  = Calendar.getInstance();
+                cal.set(cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH),
+                        timePicker.getHour(),
+                        timePicker.getMinute(),
+                        0
+                );
+
+                setAlarm(cal.getTimeInMillis());
+            }
+
+
+
+        });
+    }
+    //called when the alarm executes
+    private void setAlarm(long timeInMillis) {
+        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, Alarm.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0, intent, 0);
+        alarm.setRepeating(AlarmManager.RTC, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        Toast.makeText(this, "Alarm set!", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
